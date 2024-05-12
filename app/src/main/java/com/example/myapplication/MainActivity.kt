f8d81814a5e8881aee49a58935386b35da6e3444
projectscan <arhipovk207@gmail,com>
2024-05-13 00:02:09 +0300

    navBar(test)

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : AppCompatActivity() {
         Log.d("My @log", "Hi")
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
+        //я долблю собак а хотелось бы диму
 
 
         bindingClass.btResult.setOnClickListener {
