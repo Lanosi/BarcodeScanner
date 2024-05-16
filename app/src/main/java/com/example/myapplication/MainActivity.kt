2393b73a9ebb424672fe18dc09789b8a26229a8a
projectscan <arhipovk207@gmail,com>
2024-05-16 03:56:08 +0300

    create navigation bar + dark/bright theme switch button

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.SharedPreferences
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -10,7 +12,11 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
+import android.widget.CompoundButton
+import android.widget.Switch
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.ContextCompat
 import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
@@ -20,18 +26,53 @@ class MainActivity : AppCompatActivity() {
     lateinit var bindingClass : ActivityMainBinding
 
 
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    private lateinit var  switchTheme:Switch
+    private lateinit var textView:TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d("My @log", "Hi")
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
-        replaceFragment(Home())
+        switchTheme = findViewById(R.id.switch_theme)
+        textView=findViewById(R.id.textView)
+        switchTheme.isChecked=getSavedThemeState()
+
+        switchTheme.setOnCheckedChangeListener {_, isChecked ->
+            if (isChecked){
+                setDarkTheme()
+            }
+            else {
+                setLightTheme()
+            }
+            saveThemeState(isChecked)
+        }
+        if(switchTheme.isChecked){
+            setDarkTheme()
+        }
+        else{
+            setLightTheme()
+        }
+
+
+
+
+
+
+
         bindingClass.bottomNavigationView.setOnItemSelectedListener {
+
             when (it.itemId) {
+
                 R.id.home -> replaceFragment(Home())
                 R.id.settings -> replaceFragment(Settings())
+
                 else -> {
+
                 }
+
             }
             true
         }
@@ -78,6 +119,34 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+    private fun setLightTheme(){
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+    private fun setDarkTheme(){
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+
+    }
+    private fun getSavedThemeState():Boolean{
+        val sharedPreferences : SharedPreferences = getSharedPreferences("Theme Prefs", Context.MODE_PRIVATE)
+        return sharedPreferences.getBoolean("IsDarkTheme", false)
+    }
+    private fun saveThemeState(IsDarkTheme:Boolean){
+        val sharedPreferences : SharedPreferences = getSharedPreferences("Theme Prefs", Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+        editor.putBoolean("IsDarkTheme", IsDarkTheme)
+        editor.apply()
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val textColor : Int = if(switchTheme.isChecked)R.color.white else R.color.black
+        textView.setTextColor(ContextCompat.getColor(this, textColor))
+    }
+
+
+
     private fun replaceFragment(fragment : Fragment){
 
         val fragmentManager = supportFragmentManager
